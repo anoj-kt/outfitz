d9e1c3f213612fc7b2d59beb3917e2549e9b7e6d
anoj-kt <anojk28@gmail.com>
2022-10-31 14:31:50 +0100

    Add background image for categories

--- src/App.js
+++ src/App.js
@@ -4,32 +4,39 @@ const App = () => {
 
   const categories = [
     {
-      id: 1,
-      title: 'Hats'
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      id: 2,
-      title: 'Jackets'
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      id: 3,
-      title: 'Shoes'
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      id: 4,
-      title: 'Women'
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      id: 5,
-      title: 'Men'
-    },
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ]
 
   return (
     <div className="categories__container">
-      {categories.map(({title, id}) => (
+      {categories.map(({title, id, imageUrl}) => (
         <div key={id} className="category__container">
-          <div className="category__img" />
+          <div className="category__img" style={{
+            backgroundImage: `url(${imageUrl})`
+          }}/>
           <div className="category__body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
