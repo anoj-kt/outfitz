a47c65a92fe256f2b86823186d4e48fc487d2a94
anoj-kt <anojk28@gmail.com>
2022-11-02 12:50:58 +0100

    Renamed provider to google provider

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,12 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { 
+  signInWithGooglePopup, 
+  createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth();
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   }
 
   return (
