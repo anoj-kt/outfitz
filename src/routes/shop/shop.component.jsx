bb10dc2038d77fdcd1fcbe35bbdfd3b9c3600291
anoj-kt <anojk28@gmail.com>
2022-11-03 13:48:26 +0100

    Added styles to shop component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,11 +3,13 @@ import { useContext } from 'react';
 import { ProductsContext } from '../../contexts/products.context';
 import ProductCard from '../../components/product-card/product-card.component';
 
+import './shop.styles.scss'
+
 const Shop = () => {
     const { products } = useContext(ProductsContext);
 
     return (
-        <div>
+        <div className='products__container'>
             {products.map((product) => (
                 <ProductCard key={product.id} product={product} />
             ))}
