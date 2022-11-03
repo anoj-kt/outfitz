459909ef719b308c6e741a61c8b5d6104da6e198
anoj-kt <anojk28@gmail.com>
2022-11-03 12:56:53 +0100

    Added AuthState listener

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,26 +1,37 @@
+import { useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { UserContext } from '../../contexts/user.context';
+
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
 import './navigation.styles.scss'
 
 const Navigation = () => {
-  return (
-    <>
-        <div className='navigation'>
-            <Link className="logo-container" to='/'>
-                <Logo className='logo' />
-            </Link>
-            <div className="nav__links-container">
-                <Link className="nav__link" to='/shop'>
-                    SHOP
+    const { currentUser } = useContext(UserContext);
+
+    return (
+        <>
+            <div className='navigation'>
+                <Link className="logo-container" to='/'>
+                    <Logo className='logo' />
                 </Link>
-                <Link className="nav__link" to='/auth'>
-                    SIGN IN
-                </Link>
+                <div className="nav__links-container">
+                    <Link className="nav__link" to='/shop'>
+                        SHOP
+                    </Link>
+                    {currentUser ? (
+                        <span className="nav__link" onClick={signOutUser}>SIGN OUT</span>
+                    ) : (
+                        <Link className="nav__link" to='/auth'>
+                            SIGN IN
+                        </Link>
+                    )}
+                </div>
             </div>
-        </div>
-        <Outlet/>
-    </>
+            <Outlet/>
+        </>
   )
 }
 
