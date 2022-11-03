18882311d331608e239fdf082af35400b8573540
anoj-kt <anojk28@gmail.com>
2022-11-03 14:01:16 +0100

    Created cart dropdown component

---
+++ src/components/card-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,12 @@
+import Button from '../button/button.component';
+
+import './cart-dropdown.styles.scss';
+
+const CartDropDown = () => {
+    return (
+        <div className="cart-dropdown__container">
+            <div className="cart-items"></div>
+            <Button>GO TO CHECKOUT</Button>
+        </div>
+    )
+}
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 
@@ -28,6 +30,7 @@ const Navigation = () => {
                             SIGN IN
                         </Link>
                     )}
+                    <CartIcon />
                 </div>
             </div>
             <Outlet/>
