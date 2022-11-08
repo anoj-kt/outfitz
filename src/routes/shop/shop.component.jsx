01ad9120c3978cb37121bb8189bc2026ab3e470f
anoj-kt <anojk28@gmail.com>
2022-11-08 13:23:11 +0100

    Added nested routes in Shop component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,23 +1,17 @@
-import { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import { CategoriesContext } from '../../contexts/categories.context';
-
-import CategoryPreview from '../../components/category-preview.component/category-preview.component';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 import './shop.styles.scss'
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
 
     return (
-        <div className='shop__container'>
-        {
-            Object.keys(categoriesMap).map(title => {
-                const products = categoriesMap[title];
-                return <CategoryPreview key={title} title={title} products={products}/>
-            })
-        }
-        </div>
+        <Routes>
+            <Route index element={< CategoriesPreview />}></Route>
+            <Route path=':category' element={<Category />}></Route>
+        </Routes>
     )
 }
 
