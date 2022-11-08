20ea85e7e465ee2bd6d4a3bd9199b1957fd2b5f8
anoj-kt <anojk28@gmail.com>
2022-11-08 10:35:29 +0100

    Added decrement/increment functions for checkout page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -7,9 +7,11 @@ import './checkout-item.styles.scss';
 const CheckoutItem = ({ cartItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
 
-    const { clearItemFromCart } = useContext(CartContext);
+    const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(CartContext);
 
     const clearItemHandler = () => clearItemFromCart(cartItem);
+    const addItemHandler = () => addItemToCart(cartItem);
+    const removeItemHandler = () => removeItemFromCart(cartItem);
 
     return (
         <div className="checkout-item__container">
@@ -17,7 +19,17 @@ const CheckoutItem = ({ cartItem }) => {
                 <img src={imageUrl} alt={name} />
             </div>
             <span className='name'>{name}</span>
-            <span className='quantity'>{quantity}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={removeItemHandler}>
+                    &#10094;
+                </div>
+                <span className='value'>
+                    {quantity}
+                </span>
+                <div className='arrow'  onClick={addItemHandler}>
+                    &#10095;
+                </div>
+            </span>
             <span className='price'>{price}</span>
             <div className='remove-button' onClick={clearItemHandler}>&#10005;</div>
         </div>
