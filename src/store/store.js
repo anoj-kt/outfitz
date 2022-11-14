62c55ce2a505e564c14b8f5b2a9db8e4ee2bb36d
anoj-kt <anojk28@gmail.com>
2022-11-14 11:27:25 +0100

    Converted userContext to Redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,13 @@
 import { 
     compose,
-    configureStore,
+    createStore,
     applyMiddleware
 } from 'redux';
 import { logger } from 'redux-logger';
 
+import { rootReducer } from './root-reducer';
+
+const middleWares = [logger];
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
