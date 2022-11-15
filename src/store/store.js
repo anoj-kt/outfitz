f20939601007b72a5ab698e6265d9c41974cb03b
anoj-kt <anojk28@gmail.com>
2022-11-15 13:42:40 +0100

    Added Redux Persist to store cart to local storage

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,24 @@ import {
     createStore,
     applyMiddleware
 } from 'redux';
-import { logger } from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+// import { logger } from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const middleWares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+// const middleWares = [logger];
+// const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer);
+// export const store = createStore(rootReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store)
