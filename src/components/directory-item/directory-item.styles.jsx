d23d63f5c00d8a9a03807aa7e77d9eb399c6e4d6
anoj-kt <anojk28@gmail.com>
2022-12-05 14:04:46 +0100

    Remove borders

--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -15,7 +15,6 @@ export const Body = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border: 1px solid black;
   background-color: white;
   opacity: 0.7;
   position: absolute;
@@ -41,7 +40,6 @@ export const DirectoryItemContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  border: 1px solid black;
   margin: 0 7.5px 15px;
   overflow: hidden;
 
