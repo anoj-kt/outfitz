cafb3638ae20eb9581af5ab0f4f49c7c8f3a9460
anoj-kt <anojk28@gmail.com>
2022-12-05 13:38:50 +0100

    Added link to more categories in banner

--- src/components/banner-home/banner-home.styles.jsx
+++ src/components/banner-home/banner-home.styles.jsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
-import bannerImage from '../../assets/banner-home8.jpg'
+import { HashLink } from 'react-router-hash-link';
+
+import bannerImage from '../../assets/banner-home8.jpg';
+import { ReactComponent as DownArrowSvg }from '../../assets/down-arrow.svg';
 
 export const BannerContainer = styled.div`
     height: calc(100vh - 80px);
@@ -35,4 +38,24 @@ export const BannerButtons = styled.div`
    & > button {
     width: calc((100% - 1rem) / 2);
    }
+`;
+
+export const BannerHashLink = styled(HashLink)`
+    display: flex;
+    align-items: center;
+    color: white;
+    font-weight: 600;
+    padding: 0.25rem 0;
+    margin: 1rem 0;
+
+    & > span {
+        padding: 2px 0;
+        border-bottom: 2px solid white;
+    }
+`;
+
+export const DownArrowIcon = styled(DownArrowSvg)`
+  height: 32px;
+  width: 32px;
+  fill: white;
 `;
