857c27261e8471b6ea5c61e508b4f1281b6fce80
anoj-kt <anojk28@gmail.com>
2022-12-07 14:12:34 +0100

    Included Spinner to Shop and Category component

--- src/components/spinner/spinner.component.jsx
+++ src/components/spinner/spinner.component.jsx
@@ -1,9 +1,11 @@
 import { SpinnerContainer, SpinnerOverlay } from './spinner.styles';
 
 const Spinner = () => {
-    <SpinnerOverlay>
-        <SpinnerContainer/>
-    </SpinnerOverlay>
+    return (
+        <SpinnerOverlay>
+            <SpinnerContainer/>
+        </SpinnerOverlay>
+    )
 };
 
 export default Spinner;
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,15 +2,17 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 
-import ProductCard from '../../components/product-card/product-card.component'
+import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
@@ -20,12 +22,18 @@ const Category = () => {
     return (
         <> 
             <Title>{category.toUpperCase()}</Title>
-            <CategoryContainer>
-                {
-                    products &&
-                    products.map((product) => <ProductCard key={product.id} product={product}/>)
-                }
-            </CategoryContainer>
+            {
+                isLoading ? (
+                    <Spinner/>
+                ) : (
+                    <CategoryContainer>
+                        {
+                            products &&
+                            products.map((product) => <ProductCard key={product.id} product={product}/>)
+                        }
+                    </CategoryContainer>
+                )
+            }
         </>
     )
 }
