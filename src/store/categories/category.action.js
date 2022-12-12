6de77739b0de516d10478c33d726ad0a880c26d4
anoj-kt <anojk28@gmail.com>
2022-12-12 12:50:42 +0100

    Converted Category Redux thunk to Redux Saga

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,5 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPE } from "./category.types";
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils'
 
 export const setCategories = (categoriesArray) => 
     createAction(CATEGORIES_ACTION_TYPE.SET_CATEGORIES, categoriesArray);
@@ -13,15 +12,3 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
 
 export const fetchCategoriesFailed = (error) => 
     createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED, error);
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-    dispatch(fetchCategoriesStart());
-
-    try {
-        const categoriesArray = await getCategoriesAndDocuments('categories');
-        dispatch(fetchCategoriesSuccess(categoriesArray));
-    } catch (error) {
-        dispatch(fetchCategoriesFailed(error));
-    }
-    
-}
