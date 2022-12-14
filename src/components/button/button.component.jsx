38a5788363da4ccf3730cd9a13066c7ae1cb20d7
anoj-kt <anojk28@gmail.com>
2022-12-14 20:59:46 +0100

    Added Spinner to payment button and completed basic payment functionality

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,27 +1,31 @@
-import { 
-    BaseButton, 
-    GoogleSignInButton, 
-    InvertedButton 
+import {
+    BaseButton,
+    GoogleSignInButton,
+    InvertedButton,
+    ButtonSpinner,
 } from './button.styles';
 
 export const BUTTON_TYPE_CLASSES = {
     base: 'base',
     google: 'google-sign-in',
-    inverted: 'inverted'
-}
+    inverted: 'inverted',
+};
 
-const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => (
-    {
-        [BUTTON_TYPE_CLASSES.base]: BaseButton,
-        [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
-        [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
-    }[buttonType]
-)
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+({
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+}[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
     const CustomButton = getButton(buttonType);
 
-    return <CustomButton {...otherProps}>{children}</CustomButton>
-}
+    return (
+        <CustomButton disabled={isLoading} {...otherProps}>
+            {isLoading ? <ButtonSpinner/> : children}
+        </CustomButton>
+    );
+};
 
 export default Button;
--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,47 +1,66 @@
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useState } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { useSelector } from 'react-redux';
 
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { selectCartTotal } from '../../store/cart/cart.selector';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
-import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
+import { BUTTON_TYPE_CLASSES } from '../button/button.component';
+
+import {
+  PaymentFormContainer,
+  FormContainer,
+  PaymentButton,
+} from './payment-form.styles';
 
 const PaymentForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const amount = useSelector(selectCartTotal);
+    const currentUser = useSelector(selectCurrentUser);
+    const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
     const paymentHandler = async (e) => {
         e.preventDefault();
 
-        if(!stripe || !elements) {
+        if (!stripe || !elements) {
             return;
-        }
+        };
+
+        setIsProcessingPayment(true)
 
         const response = await fetch('/.netlify/functions/create-payment-intent', {
             method: 'post',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ amount: 10000})
-        }).then(res => res.json());
+            body: JSON.stringify({ amount: amount * 100 }),
+        }).then((res) => res.json());
 
-        const { paymentIntent: { client_secret }} = response;
+        console.log(response)
+
+        const {
+            paymentIntent: { client_secret },
+        } = response;
 
         const paymentResult = await stripe.confirmCardPayment(client_secret, {
             payment_method: {
                 card: elements.getElement(CardElement),
                 billing_details: {
-                    name: 'Anoj'
-                }
-            }
+                    name: currentUser ? currentUser.displayName : 'Guest',
+                },
+            },
         });
 
-        if(paymentResult.error) {
-            alert(paymentResult.error)
-        } else {
-            if(paymentResult.paymentIntent.status === 'succeeded') {
-                alert('Payment successful!')
-            }
-        };
+        setIsProcessingPayment(false)
 
+        if (paymentResult.error) {
+            alert(paymentResult.error);
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment successful!');
+            }
+        }
     };
 
     return (
@@ -49,10 +68,15 @@ const PaymentForm = () => {
             <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment: </h2>
                 <CardElement />
-                <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+                <PaymentButton
+                    isLoading={isProcessingPayment}
+                    buttonType={BUTTON_TYPE_CLASSES.inverted}
+                >
+                    Pay now
+                </PaymentButton>
             </FormContainer>
         </PaymentFormContainer>
-    )
+    );
 };
 
 export default PaymentForm;
