d6051295e29cf3b940d6b8dd49aec6d4063d2d7e
anoj-kt <anojk28@gmail.com>
2022-12-17 22:13:55 +0100

    Migrated Form-input component to typescript

--- src/components/form-input/form-input.component.tsx
+++ src/components/form-input/form-input.component.tsx
@@ -1,20 +1,25 @@
-import { 
-    FormInputLabel,
-    Input,
-    Group
- } from './form-input.styles.jsx'
+import { FC, InputHTMLAttributes } from 'react';
+import { FormInputLabel, Input, Group } from './form-input.styles.jsx';
 
-const FormInput = ({ label, ...otherProps }) => {
-    return (
-        <Group>
-            <Input {...otherProps}/>
-            {label && (
-            <FormInputLabel shrink={otherProps.value.length}>
-                {label}
-            </FormInputLabel>
-            )}
-        </Group>
-    )
-}
+type FormInputProps = { label: string } & InputHTMLAttributes<HTMLInputElement>;
+
+const FormInput: FC<FormInputProps> = ({ label, ...otherProps }) => {
+  return (
+    <Group>
+      <Input {...otherProps} />
+      {label && (
+        <FormInputLabel
+          shrink={Boolean(
+            otherProps.value &&
+              typeof otherProps.value === 'string' &&
+              otherProps.value.length
+          )}
+        >
+          {label}
+        </FormInputLabel>
+      )}
+    </Group>
+  );
+};
 
 export default FormInput;
