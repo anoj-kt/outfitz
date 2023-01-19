b2bdf024ac9a89d5c0a5e10a497806ee1f3a4a90
anoj-kt <anojk28@gmail.com>
2023-01-19 14:10:32 +0100

    Added UserIcon to codebase

---
+++ src/components/user-icon/user-icon.styles.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+import { ReactComponent as UserSVG } from '../../assets/user.svg';
+
+export const UserIcon = styled(UserSVG)`
+    height: 32px;
+    width: 32px;
+`;
+
+export const UserIconContainer = styled.div`
+  width: 45px;
+  height: 45px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
