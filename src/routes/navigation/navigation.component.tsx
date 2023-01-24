5d535ea811c39974c80888e706753084e11d2dc2
anoj-kt <anojk28@gmail.com>
2023-01-24 11:37:59 +0100

    Completely implemented UserIcon / UserCircle in Navbar

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -4,52 +4,45 @@ import { useSelector, useDispatch } from 'react-redux';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropDown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import { selectIsCartOpen } from '../../store/cart/cart.selector'
+import { selectIsCartOpen } from '../../store/cart/cart.selector';
 import { selectCurrentUser } from '../../store/user/user.selector';
-import { signOutStart } from '../../store/user/user.action';
 
 import { ReactComponent as Logo } from '../../assets/main-logo.svg';
 
-import { 
-    NavigationContainer,
-    LogoContainer,
-    NavLinks,
-    NavLink 
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
 } from './navigation.styles';
+
 import UserCircle from '../../components/user-circle/user-circle.component';
+import User from '../../components/user-icon/user-icon.component';
 
 const Navigation = () => {
-    const dispatch = useDispatch();
-    const currentUser = useSelector(selectCurrentUser);
-    const isCartOpen = useSelector(selectIsCartOpen);
+  const currentUser = useSelector(selectCurrentUser);
+  const isCartOpen = useSelector(selectIsCartOpen);
 
-    const signOutUser = () => dispatch(signOutStart());
+  return (
+    <>
+      <NavigationContainer>
+        <LogoContainer to='/'>
+          <Logo />
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to='/shop'>SHOP</NavLink>
+          {currentUser ? (
+            <UserCircle />
+          ) : (
+            <User />
+          )}
+          <CartIcon />
+        </NavLinks>
+        {isCartOpen && <CartDropDown />}
+      </NavigationContainer>
+      <Outlet />
+    </>
+  );
+};
 
-    return (
-        <>
-            <NavigationContainer>
-                <LogoContainer to='/'>
-                    <Logo/>
-                </LogoContainer>
-                <NavLinks>
-                    <NavLink to='/shop'>
-                        SHOP
-                    </NavLink>
-                    {currentUser ? (
-                        <NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>
-                    ) : (
-                        <NavLink to='/auth'>
-                            SIGN IN
-                        </NavLink>
-                    )}
-                    <CartIcon />
-                    <UserCircle />
-                </NavLinks>
-                {isCartOpen && <CartDropDown />}
-            </NavigationContainer>
-            <Outlet/>
-        </>
-  )
-}
-
-export default Navigation
+export default Navigation;
