f6e20e923dccb915a956420ad217c01a89f39965
anoj-kt <anojk28@gmail.com>
2023-02-06 13:43:17 +0100

    Added Payment button for checkout page

--- src/routes/checkout/checkout.styles.tsx
+++ src/routes/checkout/checkout.styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { theme } from '../../styles/theme.styles';
 
 export const CheckoutContainer = styled.div`
   width: 55%;
@@ -6,15 +7,28 @@ export const CheckoutContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 50px auto 0;
+  margin: 50px auto 2rem;
 
   h2 {
     text-transform: uppercase;
   }
+
+  button {
+    margin: .5rem 0;
+    align-self: end;
+  }
 `;
 
-export const Total = styled.span`
-  margin-top: 30px;
-  margin-left: auto;
-  font-size: 36px;
+export const TotalContainer = styled.div`
+  width: 100%;
+  margin-top: .5rem;
+  display: flex;
+  justify-content: space-between;
+  font-size: 2rem;
+
+  & :last-child {
+    color: ${theme.colors.primaryDark};
+    font-weight: 600;
+    font-size: 1.75rem;
+  }
 `;
