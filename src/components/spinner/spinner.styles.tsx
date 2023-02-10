4470781cf4464e8652fbae4dd973c5ee18ae5165
anoj-kt <anojk28@gmail.com>
2023-02-10 21:46:49 +0100

    Added top marging to footer and changed color on spinner.

--- src/components/spinner/spinner.styles.tsx
+++ src/components/spinner/spinner.styles.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { theme } from '../../styles/theme.styles';
+
 export const SpinnerOverlay = styled.div`
   height: 100vh;
   width: 100%;
@@ -12,9 +14,9 @@ export const SpinnerContainer = styled.div`
   display: inline-block;
   width: 50px;
   height: 50px;
-  border: 3px solid rgba(195, 195, 195, 0.6);
+  border: 3px solid ${theme.colors.primaryLight};
   border-radius: 50%;
-  border-top-color: #636767;
+  border-top-color: ${theme.colors.secondary};
   animation: spin 1s ease-in-out infinite;
   -webkit-animation: spin 1s ease-in-out infinite;
   @keyframes spin {
